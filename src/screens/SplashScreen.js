8d1726bb49cc73cd48ce00ae5f91213b48382357
Ashish Kumar <85387504+DecoderAshish@users.noreply.github.com>
2023-12-02 16:10:18 +0530

    Update SplashScreen.js

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,49 +1,52 @@
-// --------------------------------------------------------------------
-// SYSTEM COMPONENTS
-// --------------------------------------------------------------------
-import { View, Text, Image } from 'react-native';
-import { SafeAreaView } from 'react-native';
-import React, { useEffect } from 'react';
-// --------------------------------------------------------------------
-// COMPONENT STYLES
-// --------------------------------------------------------------------
-import { splashStyles } from '../styles/SplashStyles';
-// --------------------------------------------------------------------
-// UI COMPONENT
-// --------------------------------------------------------------------
-import Logo from '../assets/SVGComponents/Logo';
-// --------------------------------------------------------------------
-// SCREEN
-// --------------------------------------------------------------------
-import MarketScreen from './MarketScreen';
-
+import { View, Text, Image } from "react-native";
+import { SafeAreaView } from "react-native";
+import React, { useEffect } from "react";
+import { splashStyles } from "../styles/SplashStyles";
+import Logo from "../assets/SVGComponents/Logo";
 const SplashScreen = ({ navigation }) => {
-  console.log(('nnnn', navigation));
   useEffect(() => {
     splashNav();
   }, []);
 
   const splashNav = () => {
     const intervalNav = setInterval(() => {
-      navigation.replace("MarketScreen");
+      navigation.replace("Crypto");
       clearInterval(intervalNav);
     }, 3000);
   };
 
   return (
     <View className="flex-1 flex justify-center align-middle">
-      <View className="w-full h-[30%]" style={[ splashStyles.flexEnd, splashStyles.bodyColor ]}>
-        <Text style={[ splashStyles.faintedText ]} className="text-start">COIN</Text>
+      <View
+        className="w-full h-[30%]"
+        style={[splashStyles.flexStart, splashStyles.bodyColor]}
+      >
+        <Text style={[splashStyles.faintedText]} className="text-start">
+          COINS
+        </Text>
       </View>
-      <View className="w-full h-[40%]" style={[ splashStyles.flexCenter, splashStyles.bodyColor ]}>
+      <View
+        className="w-full h-[40%]"
+        style={[splashStyles.flexCenter, splashStyles.bodyColor]}
+      >
         <Logo />
-        <Text className="mt-1.5" style={[ splashStyles.normalText ]}>Coinflow</Text>
+        <Text className="mt-1.5" style={[splashStyles.normalText]}>
+          COINSWAY
+        </Text>
+        <Text className="mt-1.5" style={[splashStyles.normalText]}>
+          Made by: <Text className="font-bold">DecoderAshish</Text>
+        </Text>
       </View>
-      <View className="w-full h-[30%]" style={[ splashStyles.flexStart, splashStyles.bodyColor ]}>
-        <Text style={[ splashStyles.faintedText ]} className="text-start">FLOW</Text>
+      <View
+        className="w-full h-[30%]"
+        style={[splashStyles.flexEnd, splashStyles.bodyColor]}
+      >
+        <Text style={[splashStyles.faintedText]} className="text-start">
+          WAY
+        </Text>
       </View>
     </View>
-  ) 
-}
+  );
+};
 
-export default SplashScreen
+export default SplashScreen;
