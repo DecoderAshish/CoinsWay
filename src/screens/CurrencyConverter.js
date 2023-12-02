ad20575c0b4fbba33ac073c80f356adaae482188
Ashish Kumar <85387504+DecoderAshish@users.noreply.github.com>
2023-12-02 16:09:46 +0530

    Update CurrencyConverter.js

--- src/screens/CurrencyConverter.js
+++ src/screens/CurrencyConverter.js
@@ -11,10 +11,7 @@ import {
   TouchableOpacity,
   Image,
 } from "react-native";
-import React, {
-  useEffect,
-  useState,
-} from "react";
+import React, { useEffect, useState } from "react";
 // --------------------------------------------------------------------
 // COMPONENT STYLES
 // --------------------------------------------------------------------
@@ -23,41 +20,36 @@ import ColorTheme from "../config/ColorTheme";
 import Money from "@expo/vector-icons/Fontisto";
 
 const CurrencyConverter = () => {
-  const [cryptocurrencyPrice, setCryptocurrencyPrice] = useState(0);
-  const [conversionResultINR, setConversionResultINR] = useState(0);
+  const [conversionResultUSD, setConversionResultUSD] = useState(0);
   const [conversionResultEUR, setConversionResultEUR] = useState(0);
   const [inputError, setInputError] = useState(false);
-  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
-    fetchExchangeRate();
+    fetchExchangeRate(0);
   }, []);
 
-  const fetchExchangeRate = async () => {
+  const fetchExchangeRate = async (cryptocurrencyPrice) => {
     try {
       const response = await fetch(
-        "https://api.exchangerate-api.com/v4/latest/USD"
+        "https://api.exchangerate-api.com/v4/latest/INR"
       );
       const data = await response.json();
-      const exchangeRateINR = data.rates.INR;
+      const exchangeRateUSD = data.rates.USD;
       const exchangeRateEUR = data.rates.EUR;
-      setConversionResultINR(
-        parseFloat(cryptocurrencyPrice) * exchangeRateINR
-      );
-      setConversionResultEUR(
-        parseFloat(cryptocurrencyPrice) * exchangeRateEUR
-      );
+      setConversionResultUSD(parseFloat(cryptocurrencyPrice) * exchangeRateUSD);
+      setConversionResultEUR(parseFloat(cryptocurrencyPrice) * exchangeRateEUR);
       setInputError(false);
     } catch (error) {
       console.error("Error fetching exchange rate:", error);
     }
   };
 
-  const handleConvert = () => {
-    if (cryptocurrencyPrice !== "" && !isNaN(cryptocurrencyPrice)) {
-      fetchExchangeRate();
+  const onTextChangeCall = (value) => {
+    if (value !== "" && !isNaN(value)) {
+      fetchExchangeRate(parseFloat(value));
       setInputError(false);
-    }else{
+    } else {
+      fetchExchangeRate(0);
       setInputError(true);
     }
   };
@@ -92,37 +84,37 @@ const CurrencyConverter = () => {
           </Text>
           <View>
             <TextInput
-              onChangeText={(value) => setCryptocurrencyPrice(parseFloat(value))}
+              onChangeText={(value) => onTextChangeCall(value)}
               style={[currencyConverterStyles.input]}
               className="px-12 mt-4"
               keyboardType="numeric"
-              placeholder="enter USD value of currency"
+              placeholder="Enter INR value of currency"
               placeholderTextColor={ColorTheme.grey}
               require
             />
-            <Money
-              name="money-symbol"
-              style={[currencyConverterStyles.favIcon]}
-            />
+            <Money name="inr" style={[currencyConverterStyles.favIcon]} />
           </View>
           {inputError && (
             <Text style={currencyConverterStyles.errorText}>
               Please enter a valid numeric value.
             </Text>
           )}
-          <View>
-            <TouchableOpacity onPress={handleConvert} style={[currencyConverterStyles.submitButton, currencyConverterStyles.flexCenter]} activeOpacity={0.4} className="px-12 mt-4">
-              <Text style={[ currencyConverterStyles.submitText ]}>CONVERT</Text>
-            </TouchableOpacity>
-          </View>
           <View className="mt-8 px-5" style={[currencyConverterStyles.card]}>
             <View className="">
-              <Text style={[ currencyConverterStyles.priceTitle ]}>€ {conversionResultEUR.toFixed(2)}</Text>
-              <Text style={[ currencyConverterStyles.subTitle ]}>valuation in EUR</Text>
+              <Text style={[currencyConverterStyles.priceTitle]}>
+                € {conversionResultEUR.toFixed(2)}
+              </Text>
+              <Text style={[currencyConverterStyles.subTitle]}>
+                valuation in EUR
+              </Text>
             </View>
-            <View className="" style={[ currencyConverterStyles.flexEnd ]}>
-              <Text style={[ currencyConverterStyles.priceTitle ]}>₹ {conversionResultINR.toFixed(2)}</Text>
-              <Text style={[ currencyConverterStyles.subTitle ]}>valuation in INR</Text>
+            <View className="" style={[currencyConverterStyles.flexEnd]}>
+              <Text style={[currencyConverterStyles.priceTitle]}>
+                $ {conversionResultUSD.toFixed(2)}
+              </Text>
+              <Text style={[currencyConverterStyles.subTitle]}>
+                valuation in USD
+              </Text>
             </View>
           </View>
         </View>
